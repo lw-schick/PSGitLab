caa4c2e3f48b41cda21aaa8d094e79f43976d4ae
Leonhard Schick <leonhard.schick@lampenwelt.de>
2017-01-03 08:25:48 +0100

    use basic parsing on Invoke-WebRequest #94

--- PSGitLab/Public/Configuration/Test-GitLabAPI.ps1
+++ PSGitLab/Public/Configuration/Test-GitLabAPI.ps1
@@ -11,7 +11,7 @@
     $Domain = $GitLabConfig.Domain
     $Token = DecryptString -Token $GitLabConfig.Token
     
-    $Result = Invoke-WebRequest -Uri "$Domain/api/v3/projects?private_token=$Token"
+    $Result = Invoke-WebRequest -UseBasicParsing -Uri "$Domain/api/v3/projects?private_token=$Token"
     Remove-Variable Token
     GetGitLabStatusCode $Result.StatusCode
 }
